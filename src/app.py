2b55682d37b863715bdbf18b4c8a47c3cf0fb392
Stefanie K <stefkenyon1126@gmail.com>
2019-06-03 17:46:57 -0400

    Correct minor mistake

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 # Database Setup
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///../../Resources/hawaii.sqlite")
 
 # Reflect an existing database into a new model
 Base = automap_base()
@@ -41,26 +41,60 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """List of Precipitation"""
-    # Quwey all precipitation data
+    # Query all precipitation data
     results = session.query(Measurement.prcp).all()
 
     # Convert list of tuples into normal list
-    all_precipitation = list(np.ravel(results))
+    all_precipitations = list(np.ravel(results))
 
-    return jsonify(all_precipitation)
+    return jsonify(all_precipitations)
 
     
 @app.route("/api/v1.0/stations")
 def stations():
     """List of Weather Stations"""
+    # Query all weather station data
+    results = session.query(Station.station).all()
     
+    # Convert list of weather stations into normal list
+    all_stations = list(np.ravel(results))
+    
+    return jsonify(all_stations)
+
 @app.route("/api/v1.0/<start>")
-def vacationstartdate():
+def vacationstartdate(start):
     """Vacation Start Date Information"""
+    # Query the weather data
+    results = session.query(Measurement.prcp).all()
+    
+    # Convert list of precipitation lists into normal list
+    one_day_precipitation = list(np.ravel(results))
+    
+    # Canonicalize the start date
+    canonicalized = start.replace("-", "")
+    
+    if start == canonicalized:
+        return jsonify(one_day_precipitation)
+
+    return jsonify({"error": f"Weather for {start} not found."}), 404
 
 @app.route("/api/v1.0/<start>/<end>")
-def vacationstartandenddates():
+def vacationstartandenddates(start, end):
     """Vacation Start Date and End Date Information"""
+    # Query the weather data
+    results = session.query(Measurement.prcp).all()
+    
+    # Convert list of precipitation lists into normal list
+    trip_precipitation = list(np.ravel(results))
+    
+    # Canonicalize the start date
+    canonicalized = start.replace("-", "")
+    canonicalized = end.replace("-", "")
+    
+    if start == canonicalized and end == canonicalized:
+        return jsonify(trip_precipitation)
+
+    return jsonify({"error": f"Weather for {start} and {end} not found."}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
